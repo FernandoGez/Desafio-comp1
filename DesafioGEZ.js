0bb4d6c4a22619a818b4275b5c8e10711d131afe
unknown <FernandoMGez@outlook.com>
2022-03-02 01:31:33 -0300

    agregamos archivos

---
+++ DesafioGEZ.js
@@ -0,0 +1,17 @@
+// datos
+const proyectoFinal = [10, 6, 3, 6, 8, 2];
+
+
+// Procesamiento Datos
+for (let i = 0; i < 6; i++) {
+    if (proyectoFinal[i] >= 6) {
+        console.log(` Aprobado, Nota: ${proyectoFinal[i]} `);
+    }
+    else {
+        console.log(` Desaprobado, Nota: ${proyectoFinal[i]}`);
+    }
+
+
+}
+
+
